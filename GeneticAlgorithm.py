24002acbfe544c4386663739f53f1ae97efbe8ac
Andre <dupput@gmail.com>
2023-02-12 13:13:16 +0000

    Took some of the larger functions out and into their own files. Script can now be run successfully from run.ipynb

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -1,23 +1,15 @@
 import numpy as np
 import random
 import gym
-from tensorflow.keras.models import Sequential, save_model
-from tensorflow.keras.layers import Dense, Activation, Flatten, Input
 
-# Turn off tensorflow warnings
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+from selection import selection
+from NeuralNetwork import NeuralNetwork
 
 
-def save_agent(model, env, generation, version):
-    """ Save model function """
-    print('Saving model...')
-    save_model(model, 'Training/Saved Models/{}_{}_{}.h5'.format(env, generation, version))
-
-
-## Is this different to the Flatten function from Tensorflow?
 def flatten(self,agent):
-        """ Mutation """
+        """ Mutation 
+        Is this different to the Flatten function from Tensorflow?
+        """
         # Flatten weights
         flattened_weights = agent.model.get_weights()
         flattened_weights = [w.flatten() for w in flattened_weights]
@@ -25,86 +17,69 @@ def flatten(self,agent):
         return flattened_weights
 
 
-class NeuralNetwork():
-    """ Neural Network class """
-    def __init__(self, input_size, output_size):
-        """ Constructor 
-        :param input_size: size of the input layer
-        :param output_size: size of the output layer
-        """
-        # Input layer with input_size nodes, dense layer with 5 nodes and output layer with output_size nodes
-
-        input_layer  = Input(input_size)
-        dense_layer1 = Dense(5, activation="relu")
-        output_layer = Dense(output_size, activation="linear")
-        
-        # Assign layers to the model
-        model = Sequential()
-        model.add(input_layer)
-        model.add(dense_layer1)
-        model.add(output_layer)
-
-        # Create random weights
-        # Sets random weights to the model's weights and biases
-        weights = model.get_weights()
-        weights = [np.random.rand(*w.shape) for w in weights]
-        model.set_weights(weights)
-
-        self.model = model
-        self.layers = model.layers
-
 
 
 class GeneticAlgorithm():
     """ Genetic Algorithm class """
     def __init__(self, population_size, mutation_rate, crossover_rate, environment):
         """ Constructor 
-        :param population_size: size of the population
-        :param mutation_rate: mutation rate
-        :param crossover_rate: crossover rate
-        :param environment: Gym environment
+        
+        parameters:
+        ----------------
+            population_size: int
+                Size of the population
+            mutation_rate: float
+                Mutation rate
+            crossover_rate: float
+                Crossover rate
+            environment: str
+                Environment name corresponding to the OpenAI Gym environment
         """
         self.population_size = population_size
         self.mutation_rate = mutation_rate
         self.crossover_rate = crossover_rate
         self.environment = environment
+        self.population = self.init_population(environment)
 
 
-    def init_population(self):
+    def init_population(self, env):
         """ Initialize population
         Create a population of neural networks with random weights.
+        Input to the neural network corrsponds to the observation space.
+        Output of the neural network corresponds to the action space.
+
+
          """
         env = self.environment
 
         print('Initializing population for {}...'.format(env))
         if env == 'MountainCar-v0':
+            # agentPopulation = [NeuralNetwork(2, 3) for _ in range(self.population_size)]                            
             raise ValueError('Environment doesn"t quite work yet. Reward is always < 0 and is too random.')
-            agentPopulation = [NeuralNetwork(2, 3) for _ in range(self.population_size)]                            
         elif env == 'CartPole-v1':
             agentPopulation = [NeuralNetwork(4, 2) for _ in range(self.population_size)]
         else:
             raise ValueError('Environment not supported')
         
-        print(len(agentPopulation))
-        self.population = agentPopulation
+        return agentPopulation
 
 
-    def fitness(self):
+    def evaluate_fitness(self):
         """ 
         This function evaluates the fitness of the population. The fitness is currently the sum of rewards.
         Each agent is loaded and passed through the environment 
         :param agent: agent neural network
         """
-        # Initialize population variables
-        max_fitness = 0
-        max_agent = None
-        self.population_fitness = []
+        # Initialize population fitness list of lists.
+        # The first list contains the models
+        # The second list contains the fitness values
+        population_fitness = []
 
         # Iterate through the population
         for index, agent in enumerate(self.population):
             print('Status: {}/{}'.format(index, len(self.population)))
             
-            # Create a new environment and initialize 
+            # Create a new environment and initialize variables
             env = gym.make(self.environment)
             observation = env.reset()
             done = False
@@ -115,89 +90,21 @@ class GeneticAlgorithm():
             # Pass agent through environment. Fitness is the sum of rewards. 
             # This section can be tampered with a lot
             while not done:
-                action = np.argmax(agent.model.predict(observation.reshape(1, -1)))
+                thoughts = agent.model.predict(observation.reshape(1, -1))
+                print(thoughts)
+                action = np.argmax(thoughts)
                 observation, reward, done, info = env.step(action)
                 fitness += reward
 
             # Print fitness score
             print('Fitness: {}'.format(fitness))
+            population_fitness.append(fitness)
 
-            # Update max fitness and agent
-            if fitness > max_fitness:
-                max_fitness = fitness
-                max_agent = agent
-
-            # create a list of fitness values for the entire population
-            self.population_fitness.append(fitness)
-
-        return max_agent, max_fitness
+        # Return the population fitness
+        return population_fitness
 
 
-    def selection(self, selection_type):
-        ''' 
-        Function to select the best agents from the population. 
-
-
-
-            :param selection_type: type of selection
-            :param self.population: population of neural networks
-            :param self.population_fitness: fitness values of the population
-
-            :return: selected_population
-            :return: selected_agent
-        '''
-        # initializing a new population
-        selected_population = []
-
-        if selection_type == 'roulette_wheel':
-            """
-            Roulette wheel selection 
-            The probability of selecting an agent is proportional to its fitness value.
-            """
-            # computing probabilities for each agent
-            fitness_sum = sum(self.population_fitness)
-            self.selection_probs = [fitness / fitness_sum for fitness in self.population_fitness]
-
-            # generating a random number between 0 and 1 for the population selection
-            random_number = np.random.rand()
-
-            # selecting the population based on the random number
-            def selection(self, random_number=random_number):
-                for index, prob in enumerate(self.selection_probs):
-                    random_number -= prob
-                    # all the agents with a probability greater than the random number are selected
-                    if random_number <= 0:
-                        selected_population.append(index)
-
-                    return selected_population
-
-            selected_population = selection(self)
-
-            # a list of indices of the selected agents from the original population
-            return selected_population
-
-        
-        elif selection_type == 'tournament':
-            """
-            Tournament selection
-            Selects the best agent from a random sample of agents.
-            """
-            # chosen size to be around 20% of the population - can be changed
-            def selection(self, t_size=len(self.population)//5):
-                # selecting a random sample of indexes of agents 
-                selected_agents = random.sample(range(len(self.population)), t_size)
-
-                # selecting the best agent from the random sample
-                selected_agent = max(selected_agents, key=lambda x: self.population_fitness[x])
-
-                return selected_agent
-            
-            for _ in range(len(self.population)):
-                selected_population.append(selection(self))
-
-            # a list of indices of the selected agents from the original population
-            return selected_population
-
+    
 
     def crossover(self, selected_population):
         """ Crossover
@@ -255,19 +162,17 @@ class GeneticAlgorithm():
         gen = 0
         while gen < num_generations:
             # Initialize population
-            self.init_population()
 
             # Evaluate fitness
-            max_agent, max_fitness = self.fitness()
+            population_fitness = self.evaluate_fitness()
             
-            # max_agent.model.save('best_model.h5')  
-            # selected_population = self.selection('roulette_wheel')
-            
+            selected_population = selection(self, 'rank-based-rolette-wheel', population_fitness)
+            print(selected_population)
 
 
             # Perform crossover
             # offspring1, offspring2 = self.crossover(selected_population)
-            save_agent(max_agent.model, env, '1', 'v1')
+            # save_agent(max_agent.model, env, '1', 'v1')
             gen += 1
 
 
@@ -279,7 +184,7 @@ if __name__ == "__main__":
     env = 'CartPole-v1'
 
     # Create genetic algorithm
-    ga = GeneticAlgorithm(1, 0.1, 0.7, env)
+    ga = GeneticAlgorithm(3, 0.1, 0.7, env)
 
     # Run genetic algorithm
     ga.run(1)
