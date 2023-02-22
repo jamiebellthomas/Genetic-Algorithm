aa6945aa7a8dd2a5d469285bbf878ce5449ec9d7
pilipb <philipblecher@hotmail.com>
2023-02-22 09:47:37 +0000

    Modified sparse fitness function

--- evaluate_fitness.py
+++ evaluate_fitness.py
@@ -44,13 +44,15 @@ def sparse_rewards(observation, fitness, frontier):
 
             # as the frontier gets larger, the reward should get smaller
 
-            fitness += (abs((frontier[i][0] - observation[i])/ frontier[i][0]) + 1)**2
-            frontier[i][0] = observation[i]
+            fitness += abs((frontier[i][0] - observation[i])/ frontier[i][0])
+
+            # frontier[i][0] = observation[i]
 
         elif observation[i] > frontier[i][1] and frontier[i][1] != 0:
 
-            fitness += (abs((observation[i] - frontier[i][1])/ frontier[i][1]) + 1)**2
-            frontier[i][1] = observation[i]
+            fitness += abs((observation[i] - frontier[i][1])/ frontier[i][1])
+
+            # frontier[i][1] = observation[i]
 
     return frontier, fitness
 
@@ -153,6 +155,9 @@ def fitness_sharing(self):
 
         # retrieve the flattened weights from the agent
         chosen_agent = agent.weightsnbiases
+
+        # calculate shar_range as 10% of the length of the agent
+        shar_range = len(chosen_agent)*0.1
 
         # for each other agent in the population
         for j, other in enumerate(self.population):
